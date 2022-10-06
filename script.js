61a27f028c906250beadee4bdc173a8b48c73479
Rodrigo Yvan Caiado <106771261+Rodrigocaiado@users.noreply.github.com>
2022-10-05 22:44:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ function carregar(){
     var msg = document.getElementById('msg')
     var foto = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    var hora = 5 //data.getHours()
     msg.innerHTML = `Olá Mundo`
-    if(hora >= 0 && hora < 12){
+    if(hora >= 5 && hora < 12){
         // Manhã
         msg.innerHTML = `Bom dia... Agora são ${hora} horas.`
         foto.src = 'manha.png'
         document.body.style.background = '#e2cd9f'
-    } else if(hora >= 12 && hora < 18){
+    } else if(hora >= 12 && hora <= 18){
         // Tarde
         msg.innerHTML = `Boa tarde... Agora são ${hora} horas.`
         foto.src = 'tarde.png'
         document.body.style.background = '#b9846f'
-    } else {
+    } else if(hora > 18 && hora <= 23){
         // Noite
         msg.innerHTML = `Boa noite... Agora são ${hora} horas.`
+        foto.src = 'noite.png'
+        document.body.style.background = '#515154'
+    } else {
+        // Madrugada
+        msg.innerHTML = `Boa Madrugada... Agora são ${hora} horas.`
         foto.src = 'noite.png'
         document.body.style.background = '#515154'
     }
